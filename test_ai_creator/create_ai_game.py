9e3fee2c129ef0d74ef962476fc6eb346cf1fd70
Assaf Luz <assaf@tinytap.com>
2023-08-21 19:33:14 +0300

    pipeline push and term for test

--- test_ai_creator/create_ai_game.py
+++ test_ai_creator/create_ai_game.py
@@ -49,7 +49,7 @@ class TestCreateAiGame(TestCase):
         time.sleep(10)
 
         game_generated = False
-        timeout = 120
+        timeout = 60
         start_time = time.time()
 
         while time.time() - start_time < timeout:
