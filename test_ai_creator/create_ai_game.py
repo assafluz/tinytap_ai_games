d80ad9802b127d7f2c898a5d0e41a5498ed8ba4e
Assaf Luz <assaf@tinytap.com>
2023-10-04 15:58:10 +0300

    timeout for generating the game

--- test_ai_creator/create_ai_game.py
+++ test_ai_creator/create_ai_game.py
@@ -51,7 +51,7 @@ class TestCreateAiGame(TestCase):
         time.sleep(10)
 
         game_generated = False
-        timeout = 60
+        timeout = 200
         start_time = time.time()
 
         while time.time() - start_time < timeout:
