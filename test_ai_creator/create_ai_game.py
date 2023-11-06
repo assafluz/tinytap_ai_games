7e3b64e1412fa512eb209bac9d9c399985eb4c3d
assafluz <assaf@tinytap.com>
2023-11-06 15:27:56 +0200

    typos

--- test_ai_creator/create_ai_game.py
+++ test_ai_creator/create_ai_game.py
@@ -11,6 +11,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 class TestCreateAiGame(TestCase):
 
     def setUp(self):
